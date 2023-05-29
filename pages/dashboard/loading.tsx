6eb42970cf8ddd08945df3c686a288f7df863cde
Michael Hegner <hegner123@gmail.com>
2023-05-28 22:00:05 -0500

    updated

--- pages/dashboard/loading.tsx
+++ pages/dashboard/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-6xl">Loading...</h1>
+      <h1 className="text-6xl">Loading.. .</h1>
     </div>
   );
 }
