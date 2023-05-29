38830a530b64a09bc3de38e5ee7cfcbb9b3461f1
Michael Hegner <hegner123@gmail.com>
2023-05-28 21:48:18 -0500

    updated

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,7 +1,6 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Budget Forecast",
@@ -11,11 +10,11 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
