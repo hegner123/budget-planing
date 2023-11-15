284193c55bb133f43cc1548087519c4c1b15aaee
Michael Hegner <hegner123@gmail.com>
2023-11-15 06:04:20 -0600

    updated workspace config

---
+++ functions/deleteUser/index.ts
@@ -0,0 +1,29 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+console.log("Hello from Functions!");
+
+Deno.serve(async (req) => {
+  const { userID } = await req.json();
+  const authHeader = req.headers.get("Authorization")!;
+  const supabaseClient = createClient(
+    Deno.env.get("SUPABASE_URL") ?? "",
+    Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+    { global: { headers: { Authorization: authHeader } } }
+  );
+
+  const { data, error } = await supabaseClient.auth.admin.deleteUser(userID);
+
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" },
+  });
+});
+
+// To invoke:
+// curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
